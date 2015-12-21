3b5fe6123d0c6f5f29f430e20aff714dbda5558d
Scott Hovestadt <scott.hovestadt@gmail.com>
2015-12-21 13:44:44 -0800

    Code style: incorrect indent.

--- src/method/if.js
+++ src/method/if.js
@@ -6,7 +6,7 @@ const IfRule = require('../model/if-rule.js');
  * - gy-show-if elements are hidden by default and shown only when() = true.
  * - gy-hide-if elements are shown by default and hidden only when() = true.
  *
- * Internally, a rule's when() should return undefined when the value isn't yet known.
+ * when() should return undefined when the value isn't yet known.
  */
 const _rules = [
   { names: ['not-logged-in', 'logged-out'], when: ({ account }) => account.isInitialized() ? !account.isLoggedIn() : undefined },
@@ -16,18 +16,18 @@ const rules = _.map(_rules, (rule) => {
   rule.element = 'gy-show-if';
   return new IfRule(rule);
 }).concat(_.map(_rules, (rule) => {
-    rule.element = 'gy-hide-if';
+  rule.element = 'gy-hide-if';
 
-    // By default, when() means to show element.
-    // Reverse the value for gy-hide-if but leave undefined values untouched (do not typecast to false).
-    const when = rule.when;
-    rule.when = (params) => {
-      const res = when(params);
-      return res !== undefined ? !res : undefined; 
-    };
+  // By default, when() means to show element.
+  // Reverse the value for gy-hide-if but leave undefined values untouched (do not typecast to false).
+  const when = rule.when;
+  rule.when = (params) => {
+    const res = when(params);
+    return res !== undefined ? !res : undefined; 
+  };
 
-    return new IfRule(rule);
-  }));
+  return new IfRule(rule);
+}));
 
 /**
  * Bind to all elements in container (existing now or in future).
